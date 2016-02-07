258bd3482aa05aceaa5c4b5b659b4ab50bf59216
mz2 <matias.piipari@gmail.com>
2016-02-07 14:08:28 +0000

    Makes the build modular.

---
+++ YAML-Framework.h
@@ -0,0 +1,18 @@
+//
+//  YAML.h
+//  YAML
+//
+//  Created by Matias Piipari on 07/02/2016.
+//
+//
+
+//! Project version number for MAKVONotificationCenter.
+FOUNDATION_EXPORT double YAMLVersionNumber;
+
+//! Project version string for MAKVONotificationCenter.
+FOUNDATION_EXPORT const unsigned char YAMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MAKVONotificationCenter/PublicHeader.h>
+
+#import "YAMLSerialization.h"
+

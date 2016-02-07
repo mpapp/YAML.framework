aa435daf1cb8fe60a3c83ee196353d7897f5513e
mz2 <matias.piipari@gmail.com>
2016-02-07 14:26:07 +0000

    Xcode didn't like FOUNDATION_EXPORT.

--- YAML-Framework.h
+++ YAML-Framework.h
@@ -6,13 +6,5 @@
 //
 //
 
-//! Project version number for MAKVONotificationCenter.
-FOUNDATION_EXPORT double YAMLVersionNumber;
-
-//! Project version string for MAKVONotificationCenter.
-FOUNDATION_EXPORT const unsigned char YAMLVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <MAKVONotificationCenter/PublicHeader.h>
-
 #import "YAMLSerialization.h"
 
